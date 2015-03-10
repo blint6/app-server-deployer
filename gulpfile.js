7589a55ea5effcc5303839e6b1adea33c33de91c
blint6 <ngasull@FR404335.corp.capgemini.com>
2015-03-10 18:54:40 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var path = require('path');
+var Promise = require('rsvp').Promise;
+var gulp = require('gulp');
+var del = require('del');
+var es = require('event-stream');
+var insert = require('gulp-insert');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var changed = require('gulp-changed');
+var watch = require('gulp-watch');
+var babel = require('gulp-babel');
+var babelify = require("babelify");
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+var nodemon = require('gulp-nodemon');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+
+var getBundleName = function() {
+    var version = require('./package.json').version;
+    var name = require('./package.json').name;
+    return version + '.' + name + '.' + 'min';
+};
+
+var paths = {
+    js: ['src/server/**/*.js'],
+    asset: ['src/**', '!src/**/*.js'],
+};
+
+function bundle(bundler) {
+    return bundler.bundle()
+        // log errors if they happen
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('minode.js'))
+        // optional, remove if you dont want sourcemaps
+        .pipe(buffer())
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        })) // loads map from browserify file
+        .pipe(sourcemaps.write('./')) // writes .map file
+        //
+        .pipe(gulp.dest('./build/web'));
+}
+
+gulp.task('clean', function(cb) {
+    del.sync('build', {
+        force: true
+    }, cb);
+});
+
+gulp.task('transpile', function() {
+    function transpile(strm) {}
+
+    return gulp.src(paths.js, {
+            base: 'src'
+        })
+        .pipe(changed('build'))
+        .pipe(sourcemaps.init())
+        .pipe(insert.prepend('\'use strict\';'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(babel())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build'))
+        .pipe(livereload());
+});
+
+gulp.task('copy', function() {
+    return gulp.src(paths.asset, {
+            base: 'src'
+        })
+        .pipe(changed('build'))
+        .pipe(gulp.dest('build'))
+        .pipe(livereload());
+});
+
+gulp.task('bundle', function() {
+    var bundler = watchify(browserify('./src/web/index.js', watchify.args));
+
+    // add any other browserify options or transforms here
+    bundler
+        //.transform('brfs')
+        .transform('babelify');
+
+    gulp.task('js', bundle); // so you can run `gulp js` to build the file
+    bundler.on('update', bundle); // on any dep update, runs the bundler
+    bundler.on('log', gutil.log); // output build logs to terminal
+    bundle(bundler);
+});
+
+gulp.task('test', ['transpile'], function() {
+    return gulp
+        .src('build/**/*.spec.js', {
+            read: false
+        })
+        .pipe(mocha());
+});
+
+gulp.task('run', ['bundle', 'copy', 'transpile'], function() {
+    livereload({
+        start: true
+    });
+
+    nodemon({
+        script: './build/server/start',
+        watch: ['build', 'node_modules'],
+        ignore: 'src',
+        nodeArgs: ['--debug=9999']
+    })
+        .on('restart', function() {
+            console.log('Restarted!');
+            livereload.reload();
+        });
+});
---
+++ src/server/start.js
@@ -0,0 +1,40 @@
+let path = require('path');
+let socketio = require('socket.io');
+let express = require('express');
+let morgan = require('morgan');
+let consolidate = require('consolidate');
+
+console.info('Minode start');
+
+let app = express();
+
+// Only use logger for development environment
+if (true || process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.engine('jade', consolidate.jade);
+app.set('view engine', 'html');
+app.enable('jsonp callback');
+
+app.use('/vendor', express.static(process.cwd() + '/bower_components'));
+app.use('/script/minode.js', express.static(process.cwd() + '/build/web/minode.js'));
+
+let indexPath = path.resolve(__dirname, '../web/index.jade');
+app.use('/', function(req, res) {
+    res.render(indexPath);
+});
+// Realtime IO
+let server = app.listen(3000),
+    io = socketio.listen(server);
+
+io.on('connection', socket => {
+    console.info('Got a handshake');
+    socket.join('minode-web');
+});
+
+
+module.exports.stop = function() {
+    console.info('Minode stop');
+    return new Promise((resolve, reject) => server.close(err => err ? reject(err) : resolve()));
+};
